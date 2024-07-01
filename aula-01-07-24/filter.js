0990c0f32e92358532b8940a98231e67df82408a
marceloreggiani <mreggiani@gmail.com>
2024-07-01 11:55:58 -0300

    commit da aula do dia 01/07 inserindo o filter

---
+++ aula-01-07-24/filter.js
@@ -0,0 +1,18 @@
+// FILTER =  Cria um novo array com todos os elementos que passarem no teste da função
+
+const notas = [7.0, 4.0, 5.5, 9.0, 8.4, 10.0];
+const notasAcima7 = notas.filter((nota) => nota > 7);
+console.log(notasAcima7);
+
+// Exemplo Filtro de arquivos
+let arquivosPasta = ["codigo.js", "estilos.css", "index.html", "sobre.html"];
+let extensao = ".html";
+const arquvosHtml = arquivosPasta.filter((arquivo) => arquivo.endsWith(extensao));
+console.log(arquvosHtml);
+
+// Exemplo lista de presença
+let turma = ["José Almir", "Gabriel Braga", "Victor Icoma", "Igor Gondim", "Bismark"];
+let presenca = ["Gabriel Braga", "Victor Icoma", "Bismark"];
+
+const ausentes = turma.filter((aluno) => !presenca.includes(aluno));
+console.log(ausentes);
